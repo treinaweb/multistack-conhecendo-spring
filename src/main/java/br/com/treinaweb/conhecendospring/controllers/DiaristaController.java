b557fefa01cded1329e4e326c14f83f6bc431147
Cleyson Lima <cleysonph@gmail.com>
2021-09-06 00:08:41 -0300

    03:21 - detalhes de um diarista

--- src/main/java/br/com/treinaweb/conhecendospring/controllers/DiaristaController.java
+++ src/main/java/br/com/treinaweb/conhecendospring/controllers/DiaristaController.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.conhecendospring.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,6 +21,15 @@ public class DiaristaController {
         var modelAndView = new ModelAndView("listar_diaristas");
 
         modelAndView.addObject("diaristas", repository.findAll());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView buscarPorId(@PathVariable Long id) {
+        var modelAndView = new ModelAndView("detalhes");
+
+        modelAndView.addObject("diarista", repository.getById(id));
 
         return modelAndView;
     }
