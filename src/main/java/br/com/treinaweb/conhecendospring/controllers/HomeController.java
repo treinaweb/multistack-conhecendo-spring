a1bf2922ff4e7bc80dee025abf37b1c8488c8efc
Cleyson Lima <cleysonph@gmail.com>
2021-09-05 22:08:39 -0300

    03:15 - passando informações da view para o controller

--- src/main/java/br/com/treinaweb/conhecendospring/controllers/HomeController.java
+++ src/main/java/br/com/treinaweb/conhecendospring/controllers/HomeController.java
@@ -3,9 +3,12 @@ package br.com.treinaweb.conhecendospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.conhecendospring.dto.DiaristaForm;
 
 @Controller
 @RequestMapping("/")
@@ -30,6 +33,22 @@ public class HomeController {
         modelAndView.addObject("nome", "TreinaWeb");
 
         return modelAndView;
+    }
+
+    @GetMapping("/form")
+    public ModelAndView form() {
+        var modelAndView = new ModelAndView("form");
+
+        modelAndView.addObject("form", new DiaristaForm());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/form")
+    public String form2(DiaristaForm form) {
+        System.out.println(form);
+
+        return "redirect:/form";
     }
     
 }
---
+++ src/main/java/br/com/treinaweb/conhecendospring/dto/DiaristaForm.java
@@ -0,0 +1,79 @@
+package br.com.treinaweb.conhecendospring.dto;
+
+public class DiaristaForm {
+
+    private String nome;
+
+    private String telefone;
+
+    private String cpf;
+
+    public DiaristaForm() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DiaristaForm other = (DiaristaForm) obj;
+        if (cpf == null) {
+            if (other.cpf != null)
+                return false;
+        } else if (!cpf.equals(other.cpf))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (telefone == null) {
+            if (other.telefone != null)
+                return false;
+        } else if (!telefone.equals(other.telefone))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "DiaristaForm [cpf=" + cpf + ", nome=" + nome + ", telefone=" + telefone + "]";
+    }
+    
+}
