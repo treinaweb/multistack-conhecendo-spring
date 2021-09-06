4226695f1c1316d6292bb4b5f1c5b970d9fe54fc
Cleyson Lima <cleysonph@gmail.com>
2021-09-05 21:42:13 -0300

    03:13 - primeiro controller e primeira action

---
+++ src/main/java/br/com/treinaweb/conhecendospring/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package br.com.treinaweb.conhecendospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(name = "/", method = RequestMethod.GET)
+    public String home() {
+        return "home";
+    }
+    
+}
