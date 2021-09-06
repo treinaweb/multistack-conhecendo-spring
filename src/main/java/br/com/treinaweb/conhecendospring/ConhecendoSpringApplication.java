b0e93408dc79736185e428d1505c060d2db27a79
Cleyson Lima <cleysonph@gmail.com>
2021-09-05 21:09:53 -0300

    03:11 - criando projeto com Spring Initialzr

---
+++ src/main/java/br/com/treinaweb/conhecendospring/ConhecendoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.conhecendospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConhecendoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConhecendoSpringApplication.class, args);
+	}
+
+}
